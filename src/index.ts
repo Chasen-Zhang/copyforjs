62e71468e5670a98279315c3a5662fd357e90b93
Chasen-Zhang <787025321@qq.com>
2022-04-29 20:58:52 +0800

    feat: add copyToClipboard function

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+function noneSecurity(textToCopy: string){
+  const textArea: HTMLTextAreaElement = document.createElement('textarea');
+  textArea.value = textToCopy;
+  textArea.style.position = 'fixed';
+  textArea.style.opacity = '0';
+  textArea.style.left = '-999999px';
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  return new Promise<void>((res, rej) => {
+    document.execCommand('copy') ? res() : rej();
+    textArea.remove();
+  });
+}
+
+// copy text
+export function copyToClipboard(textToCopy: string) {
+if (navigator.clipboard && window.isSecureContext && document.execCommand) {
+  return navigator.clipboard.writeText(textToCopy);
+} else {
+  noneSecurity(textToCopy);
+}
+}
